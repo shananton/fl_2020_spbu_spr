c247b49117284332c05494a4a22556d6132dc4b3
Anton Shangareev <shananton2000@gmail.com>
2020-04-09 16:35:07 +0300

    Implement eval

--- src/LLang.hs
+++ src/LLang.hs
@@ -7,8 +7,10 @@ import Lexer hiding (Assign)
 import qualified Lexer as L
 
 import           Control.Monad       ((>=>))
+import Control.Monad.State
 import Control.Applicative
 import Data.Maybe (maybe)
+import Data.Bool (bool)
 import qualified Data.Map as Map
 
 type Expr = AST
@@ -73,4 +75,26 @@ initialConf :: [Int] -> Configuration
 initialConf input = Conf Map.empty input []
 
 eval :: LAst -> Configuration -> Maybe Configuration
-eval = error "eval not defined"
+eval p = execStateT (evalM p) where
+  evalM :: LAst -> StateT Configuration Maybe ()
+  evalM (If cond thn els) =
+    toEnum <$> evalExprM cond >>= bool (evalM els) (evalM thn)
+  evalM while@(While cond body) =
+    toEnum <$> evalExprM cond >>= (`when` (evalM body >> evalM while))
+  evalM (Assign v e) = do
+    x <- evalExprM e
+    c@Conf { subst = s } <- get
+    put $ c { subst = Map.insert v x s }
+  evalM (Read v) = do
+    c@Conf { subst = s, input = x : xs } <- get
+    put $ c { subst = Map.insert v x s, input = xs }
+  evalM (Write e) = do
+    c@Conf { output = xs } <- get
+    x <- evalExprM e
+    put $ c { output = x : xs }
+  evalM (Seq s) = foldr ((>>) . evalM) (pure ()) s
+
+  evalExprM :: Expr -> StateT Configuration Maybe Int
+  evalExprM e = do
+    s <- gets subst
+    lift $ evalExpr s e
