fc77442228707e68011a88f3c02ab7603e5ba052
Ekaterina Verbitskaia <kajigor@gmail.com>
2020-04-09 15:46:56 +0300

    Nicer Show for LAst

--- src/LLang.hs
+++ src/LLang.hs
@@ -1,8 +1,10 @@
 module LLang where
 
-import AST (AST (..), Operator (..), Subst (..))
-import Combinators (Parser (..))
-import qualified Data.Map as Map
+import           AST         (AST (..), Operator (..), Subst (..))
+import           Combinators (Parser (..))
+import           Data.List   (intercalate)
+import qualified Data.Map    as Map
+import           Text.Printf (printf)
 
 type Expr = AST
 
@@ -18,7 +20,7 @@ data LAst
   | Read { var :: Var }
   | Write { expr :: Expr }
   | Seq { statements :: [LAst] }
-  deriving (Show, Eq)
+  deriving (Eq)
 
 parseL :: Parser String String LAst
 parseL = error "parseL undefined"
@@ -28,3 +30,23 @@ initialConf input = Conf Map.empty input []
 
 eval :: LAst -> Configuration -> Maybe Configuration
 eval = error "eval not defined"
+
+instance Show LAst where
+  show =
+      go 0
+    where
+      go n t =
+        let makeIdent = if n > 0 then printf "%s|_%s" (concat $ replicate (n - 1) "| ") else id in
+
+        case t of
+          If cond thn els -> makeIdent $ printf "if %s\n%sthen\n%s\n%selse\n%s" (flatShowExpr cond) (makeIdent "") (go (ident n) thn) (makeIdent "") (go (ident n) els)
+          While cond body -> makeIdent $ printf "while %s\n%sdo\n%s" (flatShowExpr cond) (makeIdent "") (go (ident n) body)
+          Assign var expr -> makeIdent $ printf "%s := %s" var (flatShowExpr expr)
+          Read var        -> makeIdent $ printf "read %s" var
+          Write expr      -> makeIdent $ printf "write %s" (flatShowExpr expr)
+          Seq stmts       -> intercalate "\n" $ map (go n) stmts
+      ident = (+1)
+      flatShowExpr (BinOp op l r) = printf "(%s %s %s)" (flatShowExpr l) (show op) (flatShowExpr r)
+      flatShowExpr (UnaryOp op x) = printf "(%s %s)" (show op) (flatShowExpr x)
+      flatShowExpr (Ident x) = x
+      flatShowExpr (Num n) = show n
