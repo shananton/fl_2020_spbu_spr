822399a055118e13789f058e58b518517b3bb3b9
Anton Shangareev <shananton2000@gmail.com>
2020-04-16 17:12:12 +0300

    Replace src/Expr.hs

--- src/Expr.hs
+++ src/Expr.hs
@@ -1,11 +1,20 @@
 module Expr where
 
 import           AST                 (AST (..), Operator (..), Subst (..))
-import           Combinators         (Parser (..), Result (..), fail',
-                                      runParser, satisfy, stream, success)
-import           Control.Applicative
-import           Data.Char           (digitToInt, isDigit)
+import           Combinators
+import           Lexer
+
+import           Control.Applicative (many, some, (<|>))
+import           Control.Monad       ((>=>))
+import           Data.Bifunctor      (first)
+import           Data.Char           (digitToInt, isAlpha, isAlphaNum, isDigit,
+                                      isHexDigit)
+import           Data.Function       ((&))
+import           Data.Functor        (($>))
 import qualified Data.Map            as Map
+import           Data.Maybe          (fromJust, maybe)
+import           Data.Monoid         (Alt (..))
+import           Data.Tuple          (swap)
 
 data Associativity
   = LeftAssoc  -- 1 @ 2 @ 3 @ 4 = (((1 @ 2) @ 3) @ 4)
@@ -16,7 +25,40 @@ data OpType = Binary Associativity
             | Unary
 
 evalExpr :: Subst -> AST -> Maybe Int
-evalExpr = error "evalExpr undefined"
+evalExpr s = eval where
+
+  eval (Num x) = Just x
+  eval (Ident v) = Map.lookup v s
+
+  eval (UnaryOp op e) =
+    let unOps =
+          [ (Plus, id)
+          , (Minus, negate)
+          , (Not, fromEnum . (== 0))
+          ] in
+    lookup op unOps <*> eval e
+
+  eval (BinOp op el er) =
+    let binOps =
+          [ (Plus, (+))
+          , (Mult, (*))
+          , (Minus, (-))
+          , (Div, div)
+          , (Pow, (^))
+          , (Equal, fromEnum .- (==))
+          , (Nequal, fromEnum .- (/=))
+          , (Gt, fromEnum .- (>))
+          , (Ge, fromEnum .- (>=))
+          , (Lt, fromEnum .- (<))
+          , (Le, fromEnum .- (<=))
+          , (And, boolean (&&))
+          , (Or, boolean (||))
+          ] in
+    lookup op binOps <*> eval el <*> eval er
+      where
+        boolean f x y = fromEnum $ f (x /= 0) (y /= 0)
+        (.-) = (.) . (.)
+
 
 uberExpr :: Monoid e
          => [(Parser e i op, OpType)] -- список операций с их арностью и, в случае бинарных, ассоциативностью
@@ -24,38 +66,42 @@ uberExpr :: Monoid e
          -> (op -> ast -> ast -> ast) -- конструктор узла дерева для бинарной операции
          -> (op -> ast -> ast)        -- конструктор узла для унарной операции
          -> Parser e i ast
-uberExpr = error "uberExpr undefined"
+uberExpr allOps atom astBin astUn = uber allOps
+  where
+    astr l o r = astBin o l r
+    astl o r l = astBin o l r
+    uber ((op, opType):ops) = let term = uber ops in
+      case opType of
+        Binary LeftAssoc  -> foldl (&) <$> term <*> many (astl <$> op <*> term)
+        Binary RightAssoc -> flip (foldr ($)) <$> many (astr <$> term <*> op) <*> term
+        Binary NoAssoc    -> astr <$> term <*> op <*> term <|> term
+        Unary             -> flip (foldr ($)) <$> many (astUn <$> op) <*> term
+    uber [] = atom
 
+expr :: Parser String [Token] AST
+expr = uberExpr ops atom BinOp UnaryOp
+  where
+    ops = map (first listToParser)
+              [ ([Or], Binary RightAssoc)
+              , ([And], Binary RightAssoc)
+              , ([Not], Unary)
+              , ([Equal, Nequal, Lt, Le, Gt, Ge], Binary NoAssoc)
+              , ([Plus, Minus], Binary LeftAssoc)
+              , ([Mult, Div], Binary LeftAssoc)
+              , ([Plus, Minus], Unary)
+              , ([Pow], Binary RightAssoc)
+              ]
+      where
+        listToParser = fmap (getArith . getOperator) . getAlt
+          . foldMap (Alt . symbol . TOperator . Arith)
+    atom = Num . getInt <$> satisfy isInt
+        <|> Ident . getId <$> satisfy isId
+        <|> symbol (TOperator LPar) *> expr <* symbol (TOperator RPar)
 
 -- Парсер для выражений над +, -, *, /, ^ (возведение в степень)
 -- с естественными приоритетами и ассоциативностью над натуральными числами с 0.
 -- В строке могут быть скобки
 parseExpr :: Parser String String AST
-parseExpr = error "parseExpr undefined"
-
--- Парсер для целых чисел
-parseNum :: Parser String String Int
-parseNum = foldl (\acc d -> 10 * acc + digitToInt d) 0 <$> go
-  where
-    go :: Parser String String String
-    go = some (satisfy isDigit)
-
-parseNegNum :: Parser String String Int
-parseNegNum = error "parseNegNum undefined"
-
-parseIdent :: Parser String String String
-parseIdent = error "parseIdent undefined"
-
-evaluate :: String -> Maybe Int
-evaluate input = do
-  case runParser parseExpr input of
-    Success rest ast | null (stream rest) -> return $ compute ast
-    _                                     -> Nothing
-
-compute :: AST -> Int
-compute (Num x)           = x
-compute (BinOp Plus x y)  = compute x + compute y
-compute (BinOp Mult x y)  = compute x * compute y
-compute (BinOp Minus x y) = compute x - compute y
-compute (BinOp Div x y)   = compute x `div` compute y
-
+parseExpr = Parser
+  $ maybe (Failure "parseExpr failed") (Success "")
+  . (parseMaybe lexAll >=> parseMaybe (expr <* symbol (TSep Newline)))
