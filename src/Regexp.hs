abfa69e192ee1159aa57c7b666d56fbe48423256
Ekaterina Verbitskaia <kajigor@gmail.com>
2020-05-09 16:08:02 +0300

    HW11

---
+++ src/Regexp.hs
@@ -0,0 +1,20 @@
+module Regexp where
+
+import Prelude hiding (seq)
+
+data Regexp = Empty
+            | Epsilon
+            | Char Char
+            | Seq Regexp Regexp
+            | Alt Regexp Regexp
+            | Star Regexp
+            deriving (Eq, Ord)
+
+match :: Regexp -> String -> Bool
+match r s = nullable (foldl (flip derivative) r s)
+
+derivative :: Char -> Regexp -> Regexp
+derivative = error "derivative not implemented"
+
+nullable :: Regexp -> Bool
+nullable = error "nullable not implemented"
